eb1516f468971d4b142b6c6dc4455785c781f96c
* <mooaryee@st.ug.edu.gh>
2024-10-25 02:53:50 +0000

    Mark

---
+++ index.js
@@ -0,0 +1,136 @@
+
+
+
+
+
+
+
+
+
+//schema.js
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+	name: {type: String},
+        email: {type: String},
+        mobile: {type: Number},
+        description: {type: String}
+});
+
+module.export = mongoose.model("TODO", contactSchema);
+
+//controller.js
+const TODO = mongoose.model("TODO", contactSchema);
+
+const createContact = async (req, res) => {
+	try{
+		const creatingCont = await TODO.create(req.body);
+		res.status(201).send(creatingCont);
+	}catch(err){res.status(500).send(err.message);};
+
+};
+const getAllContact = async (req, res) => {
+	try{
+                const getAllCont = await TODO.find().sort( {createdAt: -1} );
+                res.status(201).send(getAllCont);
+        }catch(err){res.status(500).send(err.message);};
+};
+const getContact = async (req, res) => {
+	try{
+		if(!mongoose.Types.ObjectId.isValid(req.params)){
+			return res.status(400).send("Invalid Id");
+		};
+		const {id} = req.params;
+                const getCont = await  TODO.findById(id);
+                return res.status(200).json(getCont);
+        }catch(err){res.status(500).send(err.message);};
+};
+const updateContact = async (req, res) => {
+	try{
+		if(!mongoose.Types.ObjectId.isValid(req.params)){
+                        return res.status(400).send("Invalid Id");
+                };
+		const {id} = req.params;
+                const updateCont=await TODO.findByIdAndUpdate(id, req.body, {new: true});
+                if(!updateCont){
+			return res.status(404).send("No todo with that Id");
+		};
+		return res.status(200).json(updateCont);
+        }catch(err){res.status(500).send(err.message);};
+};
+const deleteContact = async (req, res) => {
+	try{
+                if(!mongoose.Types.ObjectId.isValid(req.params)){
+                        return res.status(400).send("Invalid Id");
+                };
+		const {id} = req.params;
+                const delCont=await TODO.findByIdAndDelete(id);
+                if(!delCont){
+                        return res.status(404).send("No todo with that Id");
+                };
+                return res.status(200).json(delCont);
+        }catch(err){res.status(500).send(err.message);};
+};
+
+module.exports = {createContact, getAllContact, getContact, updateContact, deleteContact};
+
+//router.js
+const express = require("express");
+const router = express.Router();
+
+router.post("/todo", createContact);
+router.get("/todo", getAllContact);
+router.get("/todo/:id", getContact)
+router.put("/todo/:id", updateContact);
+router.delete("/todo/:id", deleteContact);
+
+module.export = router;
+
+
+
+
+
+
+// index.js
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1/", router);
+
+mongoose.connect(process.env.MONGO_URI).then( ()=>{
+	console.log("Database Connected");
+}).catch( (err)=>{ console.log(err); } );
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+	console.log(`Serving at http://localhost/${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
